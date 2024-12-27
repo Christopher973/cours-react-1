2a9853e1c23fc04b9f4966b021afa78234d30ed4
Christopher Marie-Angélique <christopher.marieangelique.pro@gmail.com>
2024-12-27 23:52:46 +0100

    Les filtres

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,6 +4,9 @@ import Card from "./Card";
 
 const Countries = () => {
   const [data, setData] = React.useState([]);
+  const [rangeValue, setRangeValue] = React.useState(36);
+  const [selectedRadio, setSelectedRadio] = React.useState("");
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
 
   useEffect(() => {
     axios.get("https://restcountries.com/v3.1/all").then((res) => {
@@ -13,11 +16,46 @@ const Countries = () => {
 
   return (
     <div className="countries">
-      <h1>Countrie work !</h1>
-      <ul>
-        {data.map((country) => {
-          return <Card key={country.cca2} country={country} />;
+      <ul className="radio-container">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          defaultValue={rangeValue}
+          onChange={(e) => {
+            setRangeValue(e.target.value);
+          }}
+        />
+        {radios.map((continent) => {
+          return (
+            <li key={continent}>
+              <input
+                type="radio"
+                id={continent}
+                name="continentRad  io"
+                checked={continent === selectedRadio}
+                onChange={(e) => setSelectedRadio(e.target.id)}
+              />
+              <label htmlFor={continent}>{continent}</label>
+            </li>
+          );
         })}
+      </ul>
+
+      {selectedRadio && (
+        <button onClick={() => setSelectedRadio("")}>
+          Annuler la recherche
+        </button>
+      )}
+
+      <ul>
+        {data
+          .filter((country) => country.continents[0].includes(selectedRadio))
+          .sort((a, b) => b.population - a.population)
+          .slice(0, rangeValue)
+          .map((country) => {
+            return <Card key={country.cca2} country={country} />;
+          })}
       </ul>
     </div>
   );
