721b72608b713740c53e6793911f6f5ccbc1b936
Christopher Marie-Angélique <christopher.marieangelique.pro@gmail.com>
2024-12-27 20:33:56 +0100

    Complete: Router & Navigation

---
+++ src/pages/About.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>Page à propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius doloribus
+        alias quia tenetur doloremque qui aspernatur, quidem sequi dignissimos
+        reprehenderit molestiae a, iure perferendis ullam nisi voluptates
+        excepturi sunt beatae totam quas id. Perspiciatis iste exercitationem
+        officia et consequuntur itaque laboriosam debitis placeat doloremque
+        unde obcaecati ea totam aut nihil deleniti dolore suscipit quasi labore,
+        quos architecto beatae nesciunt facere autem! Corporis quaerat quibusdam
+        animi tempora dolorum, minima id alias ex molestias adipisci distinctio
+        nulla fuga perferendis, quae laboriosam magnam, nam error facilis
+        voluptates corrupti quam obcaecati rem optio! Ab nulla animi nostrum
+        fuga voluptas, officia amet deserunt suscipit optio vitae sed
+        voluptatem? Fugiat iure harum sapiente beatae ipsum, mollitia
+        exercitationem dignissimos vel vero facere adipisci ea ducimus nemo.
+        Obcaecati est necessitatibus incidunt quaerat laborum unde autem tempora
+        corrupti deleniti in quo, possimus eaque molestias excepturi? Optio,
+        nam! Eum, enim sed autem natus itaque necessitatibus. Tenetur temporibus
+        sunt neque quisquam architecto nisi consequatur reiciendis. Quis
+        voluptate a eaque. Quis ullam iste facilis ea hic neque explicabo
+        eveniet molestias veniam cum eos autem non exercitationem numquam
+        nostrum dolores, voluptatibus eligendi! Eveniet expedita cupiditate illo
+        molestias. Maiores, voluptas! Autem, sint dicta veniam obcaecati illum
+        dolores nobis ut! Veniam recusandae iure earum a.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum tempore
+        voluptatum, officia, fuga obcaecati quam saepe asperiores natus
+        accusamus maxime dolores in magnam omnis, eveniet aut distinctio cumque
+        tenetur atque? Ut odit magni sed vel tempore eveniet unde error saepe
+        voluptate sequi mollitia illo, temporibus impedit nisi! Et ipsum neque
+        rem dolorem quam vel temporibus voluptates laudantium atque quos
+        veritatis fugiat earum, doloribus veniam porro sunt. Doloremque,
+        quisquam consequuntur nostrum ut eum sequi at aliquam voluptates
+        praesentium tempora iure alias quos assumenda. Blanditiis modi
+        distinctio illo perspiciatis veniam quibusdam, voluptas quos autem quam.
+        Rerum tenetur enim unde provident, nam tempora?
+      </p>
+    </div>
+  );
+};
+
+export default About;
